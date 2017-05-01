5152348bd8fbd0a20428947b7a7d406ad0e639a7
Jon Shern <jonshern@outlook.com>
2017-04-30 22:37:10 -0500

    add logging

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
 import time
 import grovepi
-
+import atexit
+import logger
 
 
 
 def main():
+    logger = logging.getlogger('indoorhealthmonitor')
+    logger.setLevel(logging.DEBUG)
+
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler('healthmonitor.log')
+    fh.setLevel(logging.DEBUG)
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    # add the handlers to the logger
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
+    readdustsensor()
+
+
     getairqualitysensorvalue()
 
 def gettempandhumidity():
@@ -19,7 +40,21 @@ def getgasinfo():
     return 100
 
 
-    
+def readdustsensor():
+    print("Reading from the dust sensor")
+    grovepi.dust_sensor_en()
+    while True:
+        try:
+            [new_val,lowpulseoccupancy] = grovepi.dustSensorRead()
+            print grovepi.dustSensorRead()
+            if new_val:
+                print(lowpulseoccupancy)
+                logging.info('Dust Sensor Value: ' + str(lowpulseoccupancy))
+            time.sleep(5) 
+
+        except IOError:
+            print ("Error")
+        
 
 def getairqualitysensorvalue():
     air_sensor = 0
