a35a0a1fd9600e6bc605d0d5d76884f7d4b9594b
Jon Shern <jonshern@outlook.com>
2017-05-05 20:05:44 -0500

    added arg parsing and sensor testing

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import time
 import grovepi
 import atexit
-import logging
 import json
-import time.sleep as sleep
 import sys
 import sensorvalue
+import argparse
 from config import settings
 from datetime import datetime
 
@@ -14,6 +13,32 @@ from datetime import datetime
 def main():
     
     sensordatafile = getsensorfilename()
+    supportedsenors = ['Loudness', 'AirQuality','Light', 'Gas', 'TempAndHumidity']
+    
+
+
+
+    parser = argparse.ArgumentParser(
+    description='Get Sensor Data')
+    parser.add_argument(
+        '-p', '--poll', help='Poll all sensors in the config file at the configured interval', action='store_true')
+
+    parser.add_argument('-s', '--sensortest', help='Name of the sensor to test', default='nosensor')
+    args = vars(parser.parse_args())
+
+    print len(args)
+
+    if args['poll']:
+            startautopolling()
+    elif args['sensortest'] == 'nosensor':
+        print 'Please specify a sensor name'
+        print 'supported sensors:'
+        print supportedsenors
+    elif args['sensortest'] != 'nosensor':
+        sensortest(args['sensortest'])
+    else:
+        print 'No arguments were specified so nothing will be done'
+    
 
     # how do i manage the interval of all of the sensors?
     # i could just set it up as as a chron job and then either write the value to a file or a web service.
@@ -32,10 +57,29 @@ def main():
 
 
 
+def sensortest(sensorname):
+    print 'Testing the sensor' + sensorname
+
+    if sensorname == "Loudness":
+        print getloudnessinfo(1)
+    if sensorname == "AirQuality":
+        print getairqualitysensorvalue()
+    if sensorname == "Gas":
+        print getgassensorvalue("something", 1)
+    if sensorname == "TempAndHumidity":
+        print "Temperature and Humidity"
+    if sensorname == "Light":
+        print "Light Sensor"
+
+
+
+
+def startautopolling():
     while True:
         getairqualitysensorvalue()
         getgassensorvalue(location, gas_sensor_pin)
         # https://github.com/DexterInd/GrovePi/blob/master/Software/Python/grove_gas_sensor.py
+
 
 
 def getsensorfilename():
